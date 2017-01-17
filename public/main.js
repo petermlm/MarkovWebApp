038b1c8bf49a556db21b6bf9e0a02e991efd7fb1
Pedro Melgueira <pedromelgueira@gmail.com>
2017-01-17 12:31:58 +0000

    Adds first components

--- public/main.js
+++ public/main.js
@@ -8,8 +8,26 @@ export default class Main extends React.Component {
 
         this.state = {
             text: "",
-            generated_sentences: []
+            generated_sentences: [
+                "This is a test sentence",
+                "This is test another sentence"
+            ]
         };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({
+            text: event.target.value
+        });
+    }
+
+    handleSubmit(event) {
+        console.log("Submitted");
+        console.log(this.state);
+        event.preventDefault();
     }
 
     componentDidMount() {
@@ -17,9 +35,22 @@ export default class Main extends React.Component {
     }
 
     render() {
+        var gs_list = this.state.generated_sentences.map((sentence, ind) =>
+            <li key={ind}>{sentence}</li>
+        );
+
         return (
             <div>
-                Implement Me!
+            <form onSubmit={this.handleSubmit}>
+                <textarea
+                    value={this.state.text}
+                    onChange={this.handleChange}
+                />
+                <input type="submit" value="Generate" />
+            </form>
+            <div>
+            </div>
+                <ul>{gs_list}</ul>
             </div>
         );
     }
